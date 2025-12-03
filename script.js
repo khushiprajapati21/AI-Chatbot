1f423e65d8628bbc12f7c59d1abf2ceaebb95cc1
khushi prajapati <khushipraj@gmail.com>
2025-12-04 03:49:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const chatbox = document.getElementById("chat-box");
+const userInput =  document.getElementById("user-input");
+const sendBtn =  document.getElementById("send-btn");
+
+
+
+sendBtn.onClick = async () => {
+    const message = userInput.value.trim();
+    if(message == "")
+}
