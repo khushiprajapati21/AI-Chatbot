fcb78546b041a7f580d6d34f955e99a59b5279c2
khushi prajapati <khushipraj@gmail.com>
2025-12-04 19:19:21 +0530

    Initial commit

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const userInput =  document.getElementById("user-input");
 const sendBtn =  document.getElementById("send-btn");
 
 
+function addMessage(message, classNme){
+    const msgDiv = document.createElement("div");
+    msgDiv.classList.add("message",classNme);
+    msgDiv.textContent = message;
+    chatbox.appendChild(msgDiv);
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+
 
 sendBtn.onClick = async () => {
     const message = userInput.value.trim();
-    if(message == "")
-}
+    if(message == "") return;
+    addMessage(message, "user-message");
+
+    }
